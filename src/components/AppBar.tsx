0e75a6c71233ad5cf5985457da1cd1a1a5823f68
Yuta-K19418 <yuta.k.19418@gmail.com>
2022-01-14 09:51:16 +0000

    eslintのため、修正

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,16 +1,17 @@
+import React from "react";
 import { Box, Toolbar, Typography } from "@mui/material";
 import MuiAppBar from "@mui/material/AppBar";
 
 const AppBar = () => (
-    <Box sx={{ flexGrow: 1 }}>
-        <MuiAppBar position="static" style={{ backgroundColor: "#000080" }}>
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Oekaki App
-                </Typography>
-            </Toolbar>
-        </MuiAppBar>
-    </Box>
+  <Box sx={{ flexGrow: 1 }}>
+    <MuiAppBar position="static" style={{ backgroundColor: "#000080" }}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Oekaki App
+        </Typography>
+      </Toolbar>
+    </MuiAppBar>
+  </Box>
 );
 
 export default AppBar;
