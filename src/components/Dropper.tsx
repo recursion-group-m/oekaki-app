4fdecd3f1d21a01e4632f0c9c0eb1e6fda6a0748
ten66 <ikesho.1217@gmail.com>
2022-01-20 16:11:27 +0900

    #48 ToolTypeにDropperを追加

--- src/components/Dropper.tsx
+++ src/components/Dropper.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import IconButton from "@mui/material/IconButton";
 import ColorizeIcon from "@mui/icons-material/Colorize";
-import { createSvgIcon } from "@mui/material/utils";
 
 const Dropper: React.VFC = () => (
   <IconButton aria-label="dropper">
