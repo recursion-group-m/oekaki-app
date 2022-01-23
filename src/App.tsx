4cd85a2ae0de52db233fba97adfb584458bc6137
ten66 <ikesho.1217@gmail.com>
2022-01-23 19:43:13 +0900

    #51 themeの設定を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 import Top from "./pages/Top";
 import Main from "./pages/Main";
 
+const theme = createTheme({
+  palette: {
+    warning: {
+      main: "#eafdb4",
+    },
+    secondary: {
+      main: "#6c63ff",
+    },
+  },
+});
+
 const App = () => (
   <div style={{ textAlign: "center", height: "100%" }}>
-    <Link to="/">Top</Link> /<Link to="/main">Main</Link>
+    <ThemeProvider theme={theme}>
+      <Grid
+        container
+        component="main"
+        sx={{
+          height: "100vh",
+          p: 3,
+          background: theme.palette.warning.main,
+        }}
+      >
+        <Grid>Oekaki App みんなで遊ぼう！お絵かきアプリ！ 始める！</Grid>
+        <Grid>imgおくところ</Grid>
+      </Grid>
+    </ThemeProvider>
+
     <Routes>
       <Route path="/" element={<Top />} />
       <Route path="/main" element={<Main />} />
