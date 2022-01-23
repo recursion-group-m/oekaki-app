6c97d8681175f315858fe30c128a058ef0d33f93
KoheiIshida <sscsk2224@gmail.com>
2022-01-23 18:17:22 +0900

    #57 レイアウトの大枠（レスポンシブ未対応）

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -3,6 +3,16 @@ import React, { useRef, useState } from "react";
 import { Layer, Line, Stage } from "react-konva";
 import shortid from "shortid";
 // import AutoFixNormalIcon from '@mui/icons-material/AutoFixNormal';
+import Stack from "@mui/material/Stack";
+import Grid from "@mui/material/Grid";
+import Input from "@mui/material/Input";
+import CallMissedOutgoingIcon from "@mui/icons-material/CallMissedOutgoing";
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Fab from "@mui/material/Fab";
+import NavigationIcon from "@mui/icons-material/Navigation";
+
 import Undo from "./Undo";
 import Redo from "./Redo";
 import Pen from "./Pen";
@@ -97,21 +107,17 @@ const Canvas: React.VFC<Props> = (props) => {
   };
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: 1000, height: 800 }}>
-      <div style={{ display: "flex", flexDirection: "column", width: "80%", height: "80%" }}>
-        <div style={{ display: "flex", alignItems: "center", paddingBottom: "3rem" }}>
-          <Undo onClick={handleUndo} />
-          <Redo onClick={handleRedo} />
-        </div>
-        <div style={{ width: "90%", height: "80%" }}>
+    <Stack direction={{ xs: "column", md: "row" }} sx={{ justifyContent: "space-between", height: "95%" }}>
+      <Grid sm={8} sx={{ borderRight: 1, height: "100%", justifyContent: "center", alignItems: "center" }} container>
+        <Grid item>
           <Stage
             ref={stageRef}
             onMouseDown={handleMouseDown}
             onMousemove={handleMouseMove}
             onMouseup={handleMouseUp}
-            width={720}
+            width={1000}
             height={600}
-            style={{ boxShadow: "10px 5px 5px gray" }}
+            style={{ boxShadow: "10px 5px 5px gray", border: "1px solid" }}
           >
             <Layer>
               {lines.map((line) => (
@@ -128,34 +134,77 @@ const Canvas: React.VFC<Props> = (props) => {
               ))}
             </Layer>
           </Stage>
-        </div>
-      </div>
-      <Pen
-        onClick={() => {
-          handleChangeToolType("pen");
-        }}
-      />
-      <Eraser
-        onClick={() => {
-          handleChangeToolType("eraser");
-        }}
-      />
-      <Dropper
-        onClick={() => {
-          handleChangeToolType("dropper");
-        }}
-      />
-      <LineWidth
-        width={lineWidth}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLineWidth(+e.target.value)}
-      />
-      <div>
-        <ColorPalette
-          lineColor={lineColor}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLineColor(e.target.value)}
-        />
-      </div>
-    </div>
+        </Grid>
+        <Grid sm={10} sx={{ border: 1 }} item>
+          <Grid sx={{ justifyContent: "space-evenly", alignItems: "center" }} container>
+            <Grid item>
+              <Pen
+                onClick={() => {
+                  handleChangeToolType("pen");
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <Eraser
+                onClick={() => {
+                  handleChangeToolType("eraser");
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <Dropper
+                onClick={() => {
+                  handleChangeToolType("dropper");
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <LineWidth
+                width={lineWidth}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLineWidth(+e.target.value)}
+              />
+            </Grid>
+
+            <Grid item>
+              <div>
+                <ColorPalette
+                  lineColor={lineColor}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLineColor(e.target.value)}
+                />
+              </div>
+            </Grid>
+            <Grid item>
+              <div>
+                <Undo onClick={handleUndo} />
+                <Redo onClick={handleRedo} />
+              </div>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid sm={3} sx={{ borderLeft: 1, justifyContent: "center", height: "100%" }} container>
+        <Grid sm={12} sx={{ height: "100%" }} item>
+          <Paper elevation={3} sx={{ bgcolor: "#FFFBD5", color: "#5D639E" }}>
+            <h1>??????</h1>
+          </Paper>
+
+          <Grid sx={{ height: "90%", justifyContent: "start", px: "2rem" }} container>
+            <Grid sm={8} sx={{ height: "90%" }} item>
+              <Paper elevation={2} sx={{ borderRadius: "10%", height: "3rem" }}>
+                <h3>answers</h3>
+              </Paper>
+            </Grid>
+            <Grid sm={12} item>
+              <Input style={{ color: "#5D639E" }} placeholder="答えは6文字" />
+              <Fab color="secondary">
+                <NavigationIcon />
+              </Fab>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Stack>
   );
 };
 
