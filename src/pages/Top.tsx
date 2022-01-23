292bab85f0d81186f7f2732acd10ccde957c77b8
ten66 <ikesho.1217@gmail.com>
2022-01-23 21:26:07 +0900

    #51 googlefontsのlinkを追加,Topコンポーネントの簡単なレスポンシブ対応

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 const theme = createTheme({
+  typography: {
+    fontFamily: ["Neonderthaw", "cursive"].join(","),
+  },
   palette: {
     warning: {
       main: "#eafdb4",
@@ -19,15 +24,42 @@ const Top = () => (
     <Grid
       container
       component="main"
+      display="flex"
+      alignItems="center"
       sx={{
         height: "100vh",
         p: 3,
         background: theme.palette.warning.main,
       }}
     >
-      <Grid>Oekaki App</Grid>
-      <Grid>みんなで遊ぼう！お絵かきアプリ！ 始める！</Grid>
-      <Grid>imgおくところ</Grid>
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        md={5}
+        sx={{
+          height: "100%",
+        }}
+      >
+        <Box
+          sx={{
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ pb: 5 }}>
+            <Typography variant="h2">Oekaki App</Typography>
+            <Typography>みんなで遊ぼう！お絵かきアプリ！</Typography>
+          </Box>
+          <Button>はじめる</Button>
+        </Box>
+      </Grid>
+      <Grid item xs={false} sm={4} md={7}>
+        imgおくところ
+      </Grid>
     </Grid>
   </ThemeProvider>
 );
