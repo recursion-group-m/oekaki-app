c65972a006d79bc8e91ff584d6eeef3eb3a63530
Yuta-K19418 <yuta.k.19418@gmail.com>
2022-01-25 12:27:01 +0000

    djangoとのつなぎこみ

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,17 @@
 import Konva from "konva";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import { GetUserId } from "../api/users";
+import AppBar from "../components/AppBar";
 import Canvas from "../components/Canvas";
 import { LineType } from "../types";
 
 type stageType = Konva.Stage;
+
+const info = {
+  djangoWsUrl: process.env.REACT_APP_DJANGO_WS_URL || "localhost",
+};
 
 const Main = () => {
   const stageRef = useRef<stageType>(null);
@@ -22,6 +30,21 @@ const Main = () => {
   //   setLines(JSON.parse(savedJsonStringData) as LineType[]);
   //   setConfirmationState(false);
   // };
+
+  // subは
+  // const { user } = useAuth0;
+  // で置換予定
+  const sub = "test1111111111";
+  useEffect(() => {
+    const res = GetUserId(sub);
+    // ToDo: 順番
+    const roomId = uuidv4();
+    const client = new W3CWebSocket(`${info.djangoWsUrl}/${roomId}`);
+    client.onopen = () => {
+      console.log("WebSocket Client Connected");
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div style={{ width: "100%", height: "100%" }}>
