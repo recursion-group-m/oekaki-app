a0750001ccbb9def85f9c1adedcfe8a19c404da0
ten66 <ikesho.1217@gmail.com>
2022-01-25 20:04:54 +0900

    #67 不必要なコードを削除

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -8,9 +8,6 @@ const theme = createTheme({
     background: {
       default: "#eafdb4",
     },
-    success: {
-      main: "#C7E966",
-    },
     secondary: {
       main: "#6c63ff",
       dark: "#6a5acd",
