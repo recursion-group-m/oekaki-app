f146d2d48991725793336f6e95714955af792abd
ten66 <ikesho.1217@gmail.com>
2022-01-25 19:34:16 +0900

    #67 AppBarのアプリ名にトップページのリンクを追加

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Box, Toolbar, Typography } from "@mui/material";
 import MuiAppBar from "@mui/material/AppBar";
 import { ThemeProvider } from "@mui/material/styles";
@@ -9,17 +10,19 @@ const AppBar = () => (
     <Box sx={{ flexGrow: 1 }}>
       <MuiAppBar position="fixed" style={{ backgroundColor: theme.palette.success.main }}>
         <Toolbar>
-          <Typography
-            variant="h5"
-            component="div"
-            sx={{
-              flexGrow: 1,
-              color: "black",
-              display: "flex",
-            }}
-          >
-            Oekaki App
-          </Typography>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{
+                flexGrow: 1,
+                color: "black",
+                display: "flex",
+              }}
+            >
+              Oekaki App
+            </Typography>
+          </Link>
         </Toolbar>
       </MuiAppBar>
     </Box>
