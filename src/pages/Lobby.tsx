fe97973c45c34a754c8d4a1bd866b8693fe527c0
ten66 <ikesho.1217@gmail.com>
2022-01-26 16:49:20 +0900

    #52 listを作成

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,5 +1,73 @@
 import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListSubheader from "@mui/material/ListSubheader";
+import theme from "../styles";
 
-const Lobby = () => <div>LobbyPage</div>;
+const Lobby = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Grid
+      container
+      component="main"
+      display="flex"
+      sx={{
+        alignItems: { sm: "center" },
+        height: "100vh",
+        p: 3,
+      }}
+    >
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        sx={{
+          height: { xs: "75%", sm: "100%" },
+        }}
+      >
+        <Box
+          sx={{
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <List
+            sx={{
+              height: "100%",
+              width: "90%",
+              borderRadius: "1rem",
+              bgcolor: "background.paper",
+              position: "relative",
+              overflow: "auto",
+              boxShadow: 3,
+              "& ul": { p: 2 },
+            }}
+            subheader={<li />}
+          >
+            {[0, 1, 2, 3, 4].map((sectionId) => (
+              <li key={`section-${sectionId}`}>
+                <ul>
+                  <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
+                  {[0, 1, 2].map((item) => (
+                    <ListItem key={`item-${sectionId}-${item}`}>
+                      <ListItemText primary={`Item ${item}`} />
+                    </ListItem>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </List>
+        </Box>
+      </Grid>
+    </Grid>
+  </ThemeProvider>
+);
 
 export default Lobby;
