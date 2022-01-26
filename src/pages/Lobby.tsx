2c7097480bc6fa18834f6cb9b31530b13deb3aef
ten66 <ikesho.1217@gmail.com>
2022-01-26 17:19:17 +0900

    #52 listをコンポーネント化

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -8,6 +8,7 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListSubheader from "@mui/material/ListSubheader";
 import theme from "../styles";
+import UserList from "../components/UserList";
 
 const Lobby = () => (
   <ThemeProvider theme={theme}>
@@ -38,32 +39,7 @@ const Lobby = () => (
             alignItems: "center",
           }}
         >
-          <List
-            sx={{
-              height: "100%",
-              width: "90%",
-              borderRadius: "1rem",
-              bgcolor: "background.paper",
-              position: "relative",
-              overflow: "auto",
-              boxShadow: 3,
-              "& ul": { p: 2 },
-            }}
-            subheader={<li />}
-          >
-            {[0, 1, 2, 3, 4].map((sectionId) => (
-              <li key={`section-${sectionId}`}>
-                <ul>
-                  <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
-                  {[0, 1, 2].map((item) => (
-                    <ListItem key={`item-${sectionId}-${item}`}>
-                      <ListItemText primary={`Item ${item}`} />
-                    </ListItem>
-                  ))}
-                </ul>
-              </li>
-            ))}
-          </List>
+          <UserList />
         </Box>
       </Grid>
     </Grid>
