2b1ee1ded1f09bd933c2e0663d00fffb0f8b94c3
ten66 <ikesho.1217@gmail.com>
2022-01-26 19:37:34 +0900

    #52 右側のレイアウトを調整

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -6,6 +6,7 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
 import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
+import Typography from "@mui/material/Typography";
 import theme from "../styles";
 import UserList from "../components/UserList";
 
@@ -41,33 +42,58 @@ const Lobby = () => (
           <UserList />
         </Box>
       </Grid>
-      <Grid>
-        <Button
-          variant="contained"
-          size="large"
-          endIcon={<PersonAddAlt1Icon />}
+      <Grid
+        item
+        sm={6}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        sx={{
+          height: { xs: "25%", sm: "100%" },
+          width: "100%",
+        }}
+      >
+        <Box
           sx={{
-            background: theme.palette.secondary.main,
-            "&:hover": {
-              background: theme.palette.secondary.dark,
-            },
+            pb: 5,
+            display: { xs: "none", sm: "block" },
           }}
         >
-          招待
-        </Button>
-        <Button
-          variant="contained"
-          size="large"
-          endIcon={<SendIcon />}
-          sx={{
-            background: theme.palette.secondary.main,
-            "&:hover": {
-              background: theme.palette.secondary.dark,
-            },
-          }}
-        >
-          スタート
-        </Button>
+          <Typography variant="h3">Oekaki App</Typography>
+        </Box>
+        <Box display="flex">
+          <Box sx={{ pr: 1 }}>
+            <Button
+              variant="contained"
+              size="large"
+              endIcon={<PersonAddAlt1Icon />}
+              sx={{
+                background: theme.palette.secondary.main,
+                "&:hover": {
+                  background: theme.palette.secondary.dark,
+                },
+              }}
+            >
+              招待
+            </Button>
+          </Box>
+          <Box sx={{ pl: 1 }}>
+            <Button
+              variant="contained"
+              size="large"
+              endIcon={<SendIcon />}
+              sx={{
+                background: theme.palette.secondary.main,
+                "&:hover": {
+                  background: theme.palette.secondary.dark,
+                },
+              }}
+            >
+              スタート
+            </Button>
+          </Box>
+        </Box>
       </Grid>
     </Grid>
   </ThemeProvider>
