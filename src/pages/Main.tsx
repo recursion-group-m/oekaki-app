2d17a3fce3cb070af2392f960a44fa080ad9930f
Yuta-K19418 <yuta.k.19418@gmail.com>
2022-01-26 03:27:24 +0000

    ユーザーIDでroomのIDを作成する処理を実装

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,7 @@
 import Konva from "konva";
 import React, { useEffect, useRef, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
+import PostRoomId from "../api/rooms";
 import { GetUserId } from "../api/users";
 import AppBar from "../components/AppBar";
 import Canvas from "../components/Canvas";
@@ -36,13 +36,16 @@ const Main = () => {
   // で置換予定
   const sub = "test1111111111";
   useEffect(() => {
-    const res = GetUserId(sub);
-    // ToDo: 順番
-    const roomId = uuidv4();
-    const client = new W3CWebSocket(`${info.djangoWsUrl}/${roomId}`);
-    client.onopen = () => {
-      console.log("WebSocket Client Connected");
+    const openWebSocket = async () => {
+      const getResponse = GetUserId(sub);
+      const postResponse = await PostRoomId(sub);
+      const roomId = postResponse.room_id;
+      const client = new W3CWebSocket(`${info.djangoWsUrl}/${roomId}`);
+      client.onopen = () => {
+        console.log("WebSocket Client Connected");
+      };
     };
+    openWebSocket().catch((e) => console.log(e));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
