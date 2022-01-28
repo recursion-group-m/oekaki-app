1914299e077eda26bf5c68e2399593fcb739b807
KoheiIshida <sscsk2224@gmail.com>
2022-01-28 17:45:21 +0900

    #83 ボタンホバー時に説明が表示されるように修正

--- src/components/Pen.tsx
+++ src/components/Pen.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import IconButton from "@mui/material/IconButton";
 import BrushIcon from "@mui/icons-material/Brush";
+import Tooltip from "@mui/material/Tooltip";
 
 type Props = {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
 const Pen: React.VFC<Props> = ({ onClick }) => (
-  <IconButton aria-label="pen" onClick={onClick}>
-    <BrushIcon />
-  </IconButton>
+  <Tooltip title="ペン" placement="top">
+    <IconButton aria-label="pen" onClick={onClick}>
+      <BrushIcon />
+    </IconButton>
+  </Tooltip>
 );
 
 export default Pen;
