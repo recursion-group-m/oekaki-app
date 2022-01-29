d3968a0450540066a1b0e48517fa222e18d14022
Yuta-K19418 <yuta.k.19418@gmail.com>
2022-01-29 05:18:31 +0000

    Auth0Providerの実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -5,11 +6,22 @@ import App from "./App";
 
 import "./index.css";
 
+const info = {
+  domain: process.env.REACT_APP_AUTH0_DOMAIN || "localhost",
+  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID || "0",
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
+    <Auth0Provider
+      domain={info.domain}
+      clientId={info.clientId}
+      redirectUri={`${window.location.origin}${process.env.PUBLIC_URL}/lobby`}
+    >
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
