6b4cc999ec34f6853c2fd50ee6bb552f64b0b829
Yuta-K19418 <yuta.k.19418@gmail.com>
2022-01-30 08:31:12 +0000

    client、setClientの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 import Top from "./pages/Top";
 import Lobby from "./pages/Lobby";
 import Main from "./pages/Main";
 
-const App = () => (
-  <div style={{ textAlign: "center", height: "100%" }}>
-    <Routes>
-      <Route path="/" element={<Top />} />
-      <Route path="/lobby" element={<Lobby />} />
-      <Route path="/main" element={<Main />} />
-    </Routes>
-  </div>
-);
-
+const App = () => {
+  const [client, setClient] = useState<W3CWebSocket>();
+  return (
+    <div style={{ textAlign: "center", height: "100%" }}>
+      <Routes>
+        <Route path="/" element={<Top />} />
+        <Route path="/lobby" element={<Lobby client={client} setClient={setClient} />} />
+        <Route path="/main" element={<Main client={client} />} />
+      </Routes>
+    </div>
+  );
+};
 export default App;
