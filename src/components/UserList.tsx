919723f0e10949422b90e19b347c37ed582476a1
ten66 <ikesho.1217@gmail.com>
2022-01-30 18:20:12 +0900

    #93 プレイヤーリストの大枠作成

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,36 +1,64 @@
 import React from "react";
+import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import ListSubheader from "@mui/material/ListSubheader";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import ImageIcon from "@mui/icons-material/Image";
+import WorkIcon from "@mui/icons-material/Work";
+import theme from "../styles";
 
 const UserList = () => (
-  <List
+  <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    flexDirection="column"
     sx={{
       height: { xs: "90%", sm: "100%" },
       width: "90%",
       borderRadius: "1rem",
-      bgcolor: "background.paper",
-      position: "relative",
-      overflow: "auto",
       boxShadow: 3,
-      "& ul": { p: 2 },
     }}
-    subheader={<li />}
   >
-    {[0, 1, 2, 3, 4].map((sectionId) => (
-      <li key={`section-${sectionId}`}>
-        <ul>
-          <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
-          {[0, 1, 2].map((item) => (
-            <ListItem key={`item-${sectionId}-${item}`}>
-              <ListItemText primary={`Item ${item}`} />
-            </ListItem>
-          ))}
-        </ul>
-      </li>
-    ))}
-  </List>
+    <Box sx={{ p: 1 }}>プレイヤー</Box>
+    <Box
+      sx={{
+        height: "100%",
+        width: "90%",
+        borderRadius: "1rem",
+        pb: 1,
+      }}
+    >
+      <List
+        sx={{
+          height: "100%",
+          borderRadius: "1rem",
+          bgcolor: theme.palette.success.main,
+          position: "relative",
+          overflow: "auto",
+        }}
+      >
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar sx={{ color: "white" }}>
+              <ImageIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="ホスト" />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar sx={{ color: "white" }}>
+              <WorkIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Work" />
+        </ListItem>
+      </List>
+    </Box>
+  </Box>
 );
 
 export default UserList;
