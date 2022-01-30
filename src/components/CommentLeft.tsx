855d8d6333e1b85dc403560b5dbedf641fdd0092
Yuta-K19418 <yuta.k.19418@gmail.com>
2022-01-30 09:05:13 +0000

    アバターの文字、コメント内容をPropsで受け取るように修正

--- src/components/CommentLeft.tsx
+++ src/components/CommentLeft.tsx
@@ -4,27 +4,35 @@ import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
 import theme from "../styles";
 
-const CommentLeft = () => (
-  <Stack direction="row" sx={{ width: "100%", py: 2 }}>
-    <Avatar>H</Avatar>
-    <Box sx={{ width: "100%", pt: 2, pl: 2, display: "flex" }}>
-      <Box
-        sx={{
-          maxWidth: "70%",
-          borderRadius: 16,
-          bgcolor: theme.palette.background.default,
-          textAlign: "start",
-          py: 2,
-          px: 3,
-          display: "flex",
-          alignItems: "center",
-          wordBreak: "break-all",
-        }}
-      >
-        test
+type Props = {
+  initialLetter: string;
+  text: string;
+};
+
+const CommentLeft: React.VFC<Props> = (props) => {
+  const { initialLetter, text } = props;
+  return (
+    <Stack direction="row" sx={{ width: "100%", py: 2 }}>
+      <Avatar>{initialLetter}</Avatar>
+      <Box sx={{ width: "100%", pt: 2, pl: 2, display: "flex" }}>
+        <Box
+          sx={{
+            maxWidth: "70%",
+            borderRadius: 16,
+            bgcolor: theme.palette.background.default,
+            textAlign: "start",
+            py: 2,
+            px: 3,
+            display: "flex",
+            alignItems: "center",
+            wordBreak: "break-all",
+          }}
+        >
+          {text}
+        </Box>
       </Box>
-    </Box>
-  </Stack>
-);
+    </Stack>
+  );
+};
 
 export default CommentLeft;
