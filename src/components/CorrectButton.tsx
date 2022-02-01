814721b6bbedd278c65b1cebf1c556be5d69e755
KoheiIshida <sscsk2224@gmail.com>
2022-02-01 12:39:55 +0900

    #99 カラーを変更

--- src/components/CorrectButton.tsx
+++ src/components/CorrectButton.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Box from "@mui/material/Box";
 import CheckIcon from "@mui/icons-material/Check";
 import Button from "@mui/material/Button";
+import theme from "../styles";
 
 const CorrectButton = () => (
   <Box sx={{ width: "1rem", ml: 1 }}>
-    <Button variant="contained" sx={{ borderRadius: "5em" }}>
+    <Button variant="contained" sx={{ borderRadius: "5em", bgcolor: theme.palette.secondary.main }}>
       <CheckIcon />
     </Button>
   </Box>
