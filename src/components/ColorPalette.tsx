93362e71aecc2f6fc271c8af57e5ab721bdeeefe
KoheiIshida <sscsk2224@gmail.com>
2022-02-05 11:46:34 +0900

    #83 カラーピッカーを変更

--- src/components/ColorPalette.tsx
+++ src/components/ColorPalette.tsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { ColorResult, SketchPicker } from "react-color";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 type Props = {
   lineColor: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange: (event: string) => void;
 };
 
-const ColorPalette: React.VFC<Props> = ({ lineColor, onChange }) => (
-  <input type="color" value={lineColor} onChange={onChange} />
-);
+const ColorPalette: React.VFC<Props> = ({ lineColor, onChange }) => {
+  const [displayColorPicker, setDisplayColorPicker] = useState(false);
+  const handleChangeComplete = (color: ColorResult) => onChange(color.hex);
+
+  const handleClick = () => {
+    setDisplayColorPicker(!displayColorPicker);
+  };
+
+  return (
+    <div>
+      {displayColorPicker ? (
+        <Box sx={{ position: "absolute", bottom: "23%", left: "40%", zIndex: "tooltip" }}>
+          <SketchPicker color={lineColor} onChangeComplete={handleChangeComplete} />
+        </Box>
+      ) : null}
+      <Button
+        onClick={handleClick}
+        variant="contained"
+        sx={{
+          bgcolor: lineColor,
+          color: lineColor,
+          height: "100%",
+          "&:hover": { color: lineColor, bgcolor: lineColor },
+        }}
+      >
+        COLOR
+      </Button>
+    </div>
+  );
+};
 
 export default ColorPalette;
